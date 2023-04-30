8cc08b84e9c9f1c5f76acbb578640d94b01268c7
Jeffrey Zhang <jeffrey.zhang.123@gmail.com>
2023-04-29 21:16:15 -0700

    Added button handling

---
+++ script.js
@@ -0,0 +1,33 @@
+let latitude = 0;
+let longitude = 0;
+
+window.onload = function () {
+    const date = new Date();
+    const dateString = date.toLocaleString();
+    let dateElement = document.getElementById('date');
+    dateElement.textContent = dateString;
+
+    // Geolocation stuff
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(success);
+    } else {
+        console.log('Geolocation is not available in your browser.');
+    }
+
+    const btn = document.getElementById('getWeatherBtn');
+
+    btn.addEventListener("click", () => {
+        let forecast = [["M", -462], ["Tu", -462], ["W", -462], ["Th", -462], ["F", -462]];
+        let forecastElements = document.getElementsByClassName("forecast");
+        for (let i = 0; i < forecast.length; i++) {
+            forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
+        }
+    });
+}
+
+function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log('Latitude: ' + latitude);
+    console.log('Longitude: ' + longitude);
+}
