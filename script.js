806284e8d3c0ed95b1b08448f2bf00ba4a70c2ad
Jeffrey Zhang <jeffrey.zhang.123@gmail.com>
2023-05-01 17:57:08 -0700

    Fixed potential unicode problem

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ const btn = document.getElementById('getWeatherBtn');
 btn.addEventListener("click", () => {
     let forecast = [["M", -462], ["Tu", -462], ["W", -462], ["Th", -462], ["F", -462]];
     let forecastElements = document.getElementsByClassName("forecast");
+    console.assert(forecast.length === forecastElements.length);
     for (let i = 0; i < forecast.length; i++) {
-        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
+        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F";
     }
 });
