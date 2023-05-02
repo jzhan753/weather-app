d53e20fd9be9b4a412b134972317ac4ef56ec7ab
Jeffrey Zhang <jeffrey.zhang.123@gmail.com>
2023-05-01 17:39:11 -0700

    Moved script location

--- script.js
+++ script.js
@@ -13,16 +13,6 @@ window.onload = function () {
     } else {
         console.log('Geolocation is not available in your browser.');
     }
-
-    const btn = document.getElementById('getWeatherBtn');
-
-    btn.addEventListener("click", () => {
-        let forecast = [["M", -462], ["Tu", -462], ["W", -462], ["Th", -462], ["F", -462]];
-        let forecastElements = document.getElementsByClassName("forecast");
-        for (let i = 0; i < forecast.length; i++) {
-            forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
-        }
-    });
 }
 
 function success(position) {
@@ -31,3 +21,13 @@ function success(position) {
     console.log('Latitude: ' + latitude);
     console.log('Longitude: ' + longitude);
 }
+
+const btn = document.getElementById('getWeatherBtn');
+
+btn.addEventListener("click", () => {
+    let forecast = [["M", -462], ["Tu", -462], ["W", -462], ["Th", -462], ["F", -462]];
+    let forecastElements = document.getElementsByClassName("forecast");
+    for (let i = 0; i < forecast.length; i++) {
+        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
+    }
+});
