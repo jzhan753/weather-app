c0adb415d582903b8edca58289a955c8992aef9a
Jeffrey Zhang <jeffrey.zhang.123@gmail.com>
2023-05-15 17:39:19 -0700

    Current weather

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let latitude = 0;
 let longitude = 0;
 
+const apiURL = "http://localhost:3000"
+
 window.onload = function () {
     const date = new Date();
     const dateString = date.toLocaleString();
@@ -27,9 +29,11 @@ const btn = document.getElementById('getWeatherBtn');
 btn.addEventListener("click", () => {
     let forecast = [["M", -462], ["Tu", -462], ["W", -462], ["Th", -462], ["F", -462]];
     let forecastElements = document.getElementsByClassName("forecast");
+    let temperature = document.getElementById("temperature");
+    let weatherStatus = document.getElementById("weatherStatus");
 
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", `http://localhost:3000/5day/${latitude}/${longitude}`);
+    xhr.open("GET", `${apiURL}/5day/${latitude}/${longitude}`);
     xhr.send();
 
     xhr.onload = () => {
@@ -38,5 +42,15 @@ btn.addEventListener("click", () => {
         for (let i = 0; i < forecastElements.length; i++) {
             forecastElements[i].innerHTML = body[i]["dayName"] + ": " + body[i]["temp"] + "\u00B0F";
         }
+    };
+
+    const xhr1 = new XMLHttpRequest();
+    xhr1.open("GET", `${apiURL}/weather/${latitude}/${longitude}`);
+    xhr1.send();
+    xhr1.onload = () => {
+        const body = JSON.parse(xhr1.responseText);
+        console.log(body);
+        temperature.innerHTML = body.temperature;
+        weatherStatus.innerHTML = body.weatherStatus;
     };
 });
